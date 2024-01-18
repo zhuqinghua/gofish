334f28cce8600d25dc4854d27ca09dcaeb9ae342
zhuqinghua <31456037@qq.com>
2024-01-18 15:04:00 +0800

    undo ID define

--- common/entity.go
+++ common/entity.go
@@ -18,7 +18,7 @@ type Entity struct {
 	// ODataID is the location of the resource.
 	ODataID string `json:"@odata.id"`
 	// ID uniquely identifies the resource.
-	ID interface{} `json:"Id"`
+	ID string `json:"Id"`
 	// Name is the name of the resource or array element.
 	Name string `json:"Name"`
 	// Client is the REST client interface to the system.
